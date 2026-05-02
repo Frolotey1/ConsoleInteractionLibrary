5a5dfeaa71bce8d1bc1d92ae598f6deba04dd292
fr1zb3e <168219806+Frolotey1@users.noreply.github.com>
2026-05-02 19:50:22 +0500

    Удаленная директория с прошлого проекта

--- src/ConsoleOutput.cpp
+++
@@ -1,9 +0,0 @@
-#include "../include/ConsoleOutput.h"
-
-void ConsoleOutput::write(const std::string& context) {
-    std::cout << context;
-}
-
-void ConsoleOutput::write_line(const std::string& context) {
-    std::cout << context << std::endl;
-}
